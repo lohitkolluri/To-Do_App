7202def978ee8c114dceaf7c7990d745717ebfdd
Lohit Kolluri <lohitkolluri@gmail.com>
2023-12-31 15:55:37 +0530

    Add README.md & Improve App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+    </div>
+  );
+}
+
+export default App;
